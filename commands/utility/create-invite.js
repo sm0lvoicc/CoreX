2f40e4c9985c58773c0eb0aa0b34300761e2ece6
Tagliatelle <78481260+Tagliatellelol@users.noreply.github.com>
2021-06-20 20:25:30 +0400

    Add files via upload

---
+++ commands/utility/create-invite.js
@@ -0,0 +1,24 @@
+  
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  name: 'create-invite',
+  aliases: ['create-inv'],
+  timeout: '5000',
+  description: "Create an invite of the server",
+  usage: '',
+  run: async(client, message, args) => {
+    if(!message.guild.me.hasPermission("CREATE_INSTANT_INVITE")) return message.channel.send(`I do not have the permission \`CREATE_INSTANT_INVITE\``)
+    const invite = await message.channel.createInvite({
+      temporary: false,
+      maxAge: 0,
+      maxUses: 100,
+      reason: `Created by: ${message.author.tag}`,
+    })
+    await message.channel.send(new MessageEmbed()
+      .setTitle(`Created Invite`)
+      .setDescription(`Invite: https://discord.gg/${invite.code}\n\nTemporary: \`false\`\nExpires: \`No Expiry\`\nMaximum Users: \`100 users\``)
+      .setColor("BLUE")
+    )
+  }
+}
