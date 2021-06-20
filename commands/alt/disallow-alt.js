a17fa35edeb37baf1a3028832d50e8f474e49278
Tagliatellelol <mohd.a.nada@gmail.com>
2021-06-20 21:05:28 +0400

    Create disallow-alt.js

---
+++ commands/alt/disallow-alt.js
@@ -0,0 +1,44 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const schema = require('../../models/alt')
+
+module.exports = {
+    name: 'disallow-alt',
+    description: 'Remove a user from the alt whitelist',
+    timeout: 5000,
+    usage: '<user_id>',
+    aliases: ['d-alt'],
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        if(!message.member.hasPermission('MANAGE_GUILD')) return message.reply('You do not have the permission \`MANAGE_SERVER\`')
+
+        await schema.findOne({ Guild: message.guild.id}, async(err, data) => {
+            if(!data) return message.reply('The alt Module is disabled')
+            if(!args[0]) return message.reply('Please specify a user ID to remove from the whitelist')
+            if(isNaN(args[0])) return message.reply('The user ID must be a number')
+            if(!data.Allowed_Alts.includes(args[0])) return message.reply('This user has not been whitelisted')
+            let arr = db.allowedAlts
+            let newArr = removeA(arr, args[0])
+
+            await data.updateOne({
+                Allowed_Alts: newArr
+            })
+
+            message.channel.send(`Succesfully removed <@${args[0]}> from the whitelist`)
+            
+            function removeA(arr) {
+                var what, a = arguments, L = a.length, ax;
+                while (L > 1 && arr.length) {
+                    what = a[--L];
+                    while ((ax= arr.indexOf(what)) !== -1) {
+                        arr.splice(ax, 1);
+                    }
+                }
+                return arr;
+            }
+        })
+    }
+}
