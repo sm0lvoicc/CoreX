1b30e05146a9b38ed6815872c7cbd5545d9ab5c3
Tagliatellelol <mohd.a.nada@gmail.com>
2021-06-24 15:39:32 +0400

    fixed a bug in say command

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -4,10 +4,7 @@ module.exports = {
     usage: '<text>',
     timeout: 1000 * 3,
     run: async(client, message, args) => {
-        const regex = /<@&(\d{17,19})>/g;
         const sayMessage = args.join(" ");
-        if(sayMessage.has(regex)) return message.reply('I do not ping')
-
 
         if(!sayMessage) return message.reply('Please specify something to say')
         message.delete().catch(O_o=>{}); 
