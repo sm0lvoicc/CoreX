e3d14b20fdfd7151b10be5cae3cc977288875d01
Tagliatellelol <mohd.a.nada@gmail.com>
2021-07-01 21:58:11 +0400

    added winnie the pooh command

---
+++ commands/image/pooh.js
@@ -0,0 +1,26 @@
+const { Client, Message, MessageAttachment } = require('discord.js');
+
+module.exports = {
+    name: 'pooh',
+    description: 'Change the meaning of words using winnie the pooh meme',
+    timeout: 5000,
+    usage: '<text1> <text2>',
+    aliases: ['winnie'],
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+
+         const split = args.join(" ").split("/")
+        const text1 = split[0];
+        const text2 = split[1];
+        if(!text1 || !text2) return message.reply("You need 2 sentences separated with `/` for this to work.")
+   
+        const Image = `https://api.popcatdev.repl.co/pooh?text1=${encodeURIComponent(text1)}&text2=${encodeURIComponent(text2)}`
+        const poo = new MessageAttachment(Image, "tuxedopooh.png");
+        message.channel.send(poo);
+
+    }
+}
