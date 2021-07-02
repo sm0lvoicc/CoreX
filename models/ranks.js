58b17853237da8cbf265f5e4783f475258fc6bf6
Tagliatellelol <mohd.a.nada@gmail.com>
2021-07-02 17:07:10 +0400

    Delete ranks.js

--- models/ranks.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-let Schema = new mongoose.Schema({
-    Guild: String,
-    Rank: String,
-    Role: String
-})
-
-module.exports = mongoose.model('ranks', Schema)
