efaa58b78f52c250df55649a14e1efd8271d368a
Tagliatellelol <mohd.a.nada@gmail.com>
2021-07-05 22:15:03 +0400

    added discord-reply and changed some commands

---
+++ commands/member_system/autorole.js
@@ -0,0 +1,77 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const schema = require('../../models/autorole')
+
+module.exports = {
+    name: 'autorole',
+    description: 'Enable/Disable AutoRole',
+    timeout: 3000,
+    usage: '<enable> <@role> || <disable>',
+    aliases: ['auto-role'],
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("You do not have the permission \`MANAGE_ROLES\`")
+        if(!message.guild.me.hasPermission("MANAGE_ROLES")) return message.reply("You do not have the permission \`MANAGE_ROLES\`")
+
+        options = [
+            'enable',
+            'disable'
+        ]
+
+        if (!args.length) return message.channel.send("Please enter either **enable** or **disable**")
+        const opt = args[0].toLowerCase();
+        if (!opt) return message.reply('Please enter either **enable** or **disable**')
+
+
+        if (!options.includes(opt)) return message.channel.send('Please enter either **enable** or **disable**')
+
+        if(opt == 'enable') {
+            const role = await message.mentions.roles.first() || message.guild.roles.cache.get(args[0])
+        if(!role) return message.channel.send(`Please mention a role to set as the autorole`)
+        
+        if(role.position >= message.guild.me.roles.highest.position) return message.channel.send('I cannot add a role that is higher/equal to my role')
+        
+        schema.findOne({ Guild: message.guild.id }, async(err, data) => {
+            if(err) throw err
+            if(data) {
+                data.delete()
+                const newData = new schema({
+                    Guild: message.guild.id,
+                    Role: role.id,
+                })
+                newData.save()
+                const embed2 = new MessageEmbed()
+                .setDescription(`<:corexyes:860561725916053514> Auto-role has been updated to ${role}`)
+                .setTimestamp()
+                message.channel.send(embed2)
+
+            } else {
+                data = new schema({
+                    Guild: message.guild.id,
+                    Role: role.id,
+                })
+                await data.save()
+                const embed = new MessageEmbed()
+                .setDescription(`<:corexyes:860561725916053514> Auto-role has been set to ${role}`)
+                .setTimestamp()
+                message.channel.send(embed)
+            }
+        })
+        }
+
+        if(opt == 'disable') {
+            schema.findOne({ Guild: message.guild.id }, async(err, data) => {
+                if(err) throw err
+                if(!data) {
+                    message.channel.send(`<:corexerror:860580531825147994> The Auto Role module has been disabled already`)
+                } else {
+                    await schema.findOneAndDelete({ Guild: message.guild.id })
+                    message.channel.send(`<:corexyes:860561725916053514> The Auto Role Module has been disabled`)
+                }
+            })
+        }
+    }
+}
---
+++ commands/member_system/dmjoin.js
@@ -0,0 +1,61 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const schema = require('../../models/dmjoin')
+
+
+module.exports = {
+    name: 'dmjoin',
+    description: 'Enable/Disable dmjoin.',
+    timeout: 3000,
+    usage: '<enable> <text> || <disable>',
+    aliases: ['dm-join'],
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        if(!message.member.hasPermission('MANAGE_GUILD')) return message.reply('You do not have the permission \`MANAGE_GUILD\`')
+
+        options = [
+            'enable',
+            'disable'
+        ]
+
+        if (!args.length) return message.channel.send("Please enter either **enable** or **disable**")
+        const opt = args[0].toLowerCase();
+        if (!opt) return message.reply('Please enter either **enable** or **disable**')
+
+
+        if (!options.includes(opt)) return message.channel.send('Please enter either **enable** or **disable**')
+
+        if(opt == 'enable') {
+            const text = args.slice(1).join(" ")
+
+            const noText = new MessageEmbed()
+          .setTitle(`<:corexsearch:860609884924149801> Available tags`)
+          .setDescription(` **{user}** : <@New-Member> \n **{server}** : Server Name \n **{user.tag}** : New-Member-Tag \n **{user.id}** : New-Member-ID \n **{membercount}** : Total Members`)
+          .setFooter(`More Soon`)
+          .setColor("RANDOM")
+    
+          if(!text) return message.channel.send(noText)
+    
+          schema.findOne({ Guild: message.guild.id}, async(err, data) => {
+            if(err) throw err;
+            if(data) data.delete()
+            new schema({
+              Guild: message.guild.id,
+              Text: text
+            }).save()
+            const dmjoin = text.replace(/{user}/,`<@${message.author.id}>`).replace(/{server}/,message.guild.name).replace(/{user.tag}/,message.author.tag).replace(/{user.id}/, message.author.id).replace(/{membercount}/,message.guild.memberCount)
+            message.member.send(dmjoin)
+          })
+            
+        } if (opt == 'disable') {
+            schema.findOne({ Guild: message.guild.id}, async(err, data) => {
+                if(!data) return message.channel.send('<:corexerror:860580531825147994> DM join is already disabled.')
+                data.delete()
+                message.channel.send('<:corexyes:860561725916053514> DM join has been disabled.')
+            })
+        }
+    }
+}
