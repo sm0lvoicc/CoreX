bf15e1a52c5f116c941276c1ed9e6ddaf8307cdf
Tagliatellelol <mohd.a.nada@gmail.com>
2021-07-06 19:44:04 +0400

    added lineReply to afk event

--- events/afk.js
+++ events/afk.js
@@ -8,13 +8,13 @@ client.on('message', async(message) => {
     //message get afk person
     if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
         await db.delete(`afk-${message.author.id}+${message.guild.id}`)
-        message.reply(`I have removed your AFK!`)
+        message.lineReply(`I have removed your AFK!`)
     }
     //get mentioned afk
     if(message.mentions.members.first()) {
         const info = db.get(`afk-${message.mentions.members.first().id}+${message.guild.id}`)
         if(db.has(`afk-${message.mentions.members.first().id}+${message.guild.id}`)) {
-            message.reply(`**${message.mentions.members.first().user.username}** is AFK for: **${info}**`)
+            message.lineReply(`**${message.mentions.members.first().user.username}** is AFK for: **${info}**`)
         } else return;
     }else;
 
