d08c753c80252a7ebd37b51245a685d61b77f6e6
Luxxy-GF <deniedaddicted@gmail.com>
2021-07-07 20:51:19 +0000

    Floppa :)

--- commands/fun/floppa.js
+++ commands/fun/floppa.js
@@ -8,7 +8,7 @@ module.exports = {
     userPerms: ['SEND_MESSAGES'],
     clientPerms: ['SEND_MESSAGES'],
     run: async(client, message, args) => {
-        let Embed = new MessageEmbed();
+        let Embed = new Discord.MessageEmbed();
           Embed.setTitle(`${emoji.cat} Floppa!`);
           Embed.setImage("https://media.discordapp.net/attachments/813149271805263882/814241246339399710/GIF-201208_072258.gif");
           Embed.setColor(`RANDOM`);
