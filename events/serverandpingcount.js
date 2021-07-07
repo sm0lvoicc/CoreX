a530ffd31398f452206e3a69d0b4e095ea6ad5ee
Luxxy-GF <deniedaddicted@gmail.com>
2021-07-07 16:46:27 +0000

    server and ping count for support server

---
+++ events/serverandpingcount.js
@@ -0,0 +1,12 @@
+const client = require(`../index`)
+const Discord = require(`discord.js`)
+
+client.on('ready', async() => {
+    const guild = client.guilds.cache.get('818760629920989225');
+    setInterval(() =>{
+        const channel = guild.channels.cache.get('862369853289398273');
+        const channel2 = guild.channels.cache.get('862369910969466931');
+        channel.setName(`📚 Total Servers: ${client.guilds.cache.size}`);
+        channel2.setName(`🏓 Ping: ${Math.round(client.ws.ping)}ms`);
+    }, 50000);
+})
