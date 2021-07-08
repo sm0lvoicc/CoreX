86924436086d45e6ce8f1517ebf3f6e0a69be990
Luxxy-GF <deniedaddicted@gmail.com>
2021-07-08 10:58:17 +0000

    updated server and ping count for discord api

--- events/serverandpingcount.js
+++ events/serverandpingcount.js
@@ -8,5 +8,6 @@ client.on('ready', async() => {
         const channel2 = guild.channels.cache.get('862369910969466931');
         channel.setName(`📚 Total Servers: ${client.guilds.cache.size}`);
         channel2.setName(`🏓 Ping: ${Math.round(client.ws.ping)}ms`);
-    }, 50000);
+    }, 300000); // updates every 5 mins 
+    // discord api limits 
 })
