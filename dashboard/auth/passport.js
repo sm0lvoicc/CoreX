aaa1fef2d0b18e244db650143b998aeefc316dfa
Luxxy-GF <deniedaddicted@gmail.com>
2021-07-08 17:51:09 +0000

    bug fix ;/

--- dashboard/auth/passport.js
+++ dashboard/auth/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 var DiscordStrategy = require('passport-discord').Strategy;
-const config = require('../config/config.json')
+const config = require('../config.json')
 
 module.exports = function(passport) {
     var scopes = ['identify', 'email', 'guilds', 'guilds.join'];
