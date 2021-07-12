9526631e3a7ffd27e55327b6fc99194745b9a615
Luxxy-GF <deniedaddicted@gmail.com>
2021-07-12 09:44:13 +0000

    added commands

---
+++ commands/image/toilet.js
@@ -0,0 +1,26 @@
+const Canvas = require('canvas');
+const Discord = require('discord.js');
+
+
+module.exports = {
+    name: "toilet",
+    description: "",
+    timeout: 5000,
+    userPerms: ["SEND_MESSAGES"],
+    clientPerms: ["SEND_MESSAGES"],
+    run: async (client, message, args) => {
+        const member = message.mentions.members.first() || message.member;
+        const canvas = Canvas.createCanvas(500, 670);
+        const ctx = canvas.getContext('2d');
+        const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/779441456464003122/812706484240121876/unknown.png');
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        // ctx.beginPath();
+        // // ctx.arc(350, 150, 100, 0, Math.PI * 2, true);
+        // ctx.closePath();
+        // ctx.clip();
+        const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+        ctx.drawImage(avatar, 135, 350, 205, 205);
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), `toilet_${member.user.username}.jpg`);
+        message.channel.send(attachment);
+    }
+}
