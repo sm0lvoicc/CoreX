22b341fbc5860e303c67c0161261a94bfaab8da7
TheCoderGuru <86580705+TheCoderGuru@users.noreply.github.com>
2021-07-14 10:26:57 -0400

    Added in the Starboard feature

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const fs = require("fs");
 const DiscordVoice = require("discord-voice");
 const client = new Client({
   disableMentions: "everyone",
+  partials: ['MESSAGE', 'REACTION']
 });
 const logs = require('discord-logs');
 logs(client);
@@ -119,4 +120,63 @@ client.distube
         message.channel.send("An error encountered: " + e);
     });
 
+// Starboard Feature for On Message Reaction Add
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    const handleStarboard = async () => {
+        const starboard = client.channels.cache.find(channel => channel.name.toLowerCase() === 'starboard');
+        const msgs = await starboard.messages.fetch({ limit: 100 });
+        const existingMsg = msgs.find(msg => 
+            msg.embeds.length === 1 ?
+            (msg.embeds[0].footer.text.startsWith(reaction.message.id) ? true : false) : false);
+        if(existingMsg) existingMsg.edit(`${reaction.count} - 🌟`);
+        else {
+            const embed = new MessageEmbed()
+                .setAuthor(reaction.message.author.tag, reaction.message.author.displayAvatarURL())
+                .addField('Url', reaction.message.url)
+                .setDescription(reaction.message.content)
+                .setFooter(reaction.message.id + ' - ' + new Date(reaction.message.createdTimestamp));
+            if(starboard)
+                starboard.send('1 - 🌟', embed);
+        }
+    }
+    if(reaction.emoji.name === '🌟') {
+        if(reaction.message.channel.name.toLowerCase() === 'starboard') return;
+        if(reaction.message.partial) {
+            await reaction.fetch();
+            await reaction.message.fetch();
+            handleStarboard();
+        }
+        else
+            handleStarboard();
+    }
+});
+
+// Starboard Feature for On Reaction Remove
+
+client.on('messageReactionRemove', async (reaction, user) => {
+    const handleStarboard = async () => {
+        const starboard = client.channels.cache.find(channel => channel.name.toLowerCase() === 'starboard');
+        const msgs = await starboard.messages.fetch({ limit: 100 });
+        const existingMsg = msgs.find(msg => 
+            msg.embeds.length === 1 ? 
+            (msg.embeds[0].footer.text.startsWith(reaction.message.id) ? true : false) : false);
+        if(existingMsg) {
+            if(reaction.count === 0)
+                existingMsg.delete({ timeout: 2500 });
+            else
+                existingMsg.edit(`${reaction.count} - 🌟`)
+        };
+    }
+    if(reaction.emoji.name === '🌟') {
+        if(reaction.message.channel.name.toLowerCase() === 'starboard') return;
+        if(reaction.message.partial) {
+            await reaction.fetch();
+            await reaction.message.fetch();
+            handleStarboard();
+        }
+        else
+            handleStarboard();
+    }
+});
 client.login(token);
