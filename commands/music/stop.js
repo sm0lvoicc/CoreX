7eea93ca74d358aa0bd8f9c91c481d77bb025dce
lag4lyfe <lag4lyfe.biz@gmail.com>
2021-07-15 04:56:44 +0800

    t

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,30 +1,30 @@
-const { MessageEmbed } = require("discord.js")
-module.exports = {
-    name: 'stop',
-    description: "Stops the Music & clears the queue",
-    usage: "stop",
-    aliases: [],
-    timeout: 5000,
-    userPerms: ['SEND_MESSAGES'],
-    clientPerms: ['SEND_MESSAGES'],
-    run: async(client, message, args) => {
-        if (!message.member.voice.channel) {
-            const stopError = new MessageEmbed()
-              .setDescription("You Need to be in a Voice Channel to stop Music!")
-              .setColor("RED")
-            return message.channel.send(stopError)
-        }
-        if(!client.distube.isPlaying(message)) {
-            const stopError2 = new MessageEmbed()
-            .setDescription("There is Nothing Playing")
-            .setColor("RED")
-            return message.channel.send(stopError2)
-        }
-        client.distube.stop(message);
-        const embed = new MessageEmbed()
-        .setDescription('Stopped!')
-        .setColor("BLUE")
-        message.channel.send(embed)
-
-    }
-}
+// const { MessageEmbed } = require("discord.js")
+// module.exports = {
+//     name: 'stop',
+//     description: "Stops the Music & clears the queue",
+//     usage: "stop",
+//     aliases: [],
+//     timeout: 5000,
+//     userPerms: ['SEND_MESSAGES'],
+//     clientPerms: ['SEND_MESSAGES'],
+//     run: async(client, message, args) => {
+//         if (!message.member.voice.channel) {
+//             const stopError = new MessageEmbed()
+//               .setDescription("You Need to be in a Voice Channel to stop Music!")
+//               .setColor("RED")
+//             return message.channel.send(stopError)
+//         }
+//         if(!client.distube.isPlaying(message)) {
+//             const stopError2 = new MessageEmbed()
+//             .setDescription("There is Nothing Playing")
+//             .setColor("RED")
+//             return message.channel.send(stopError2)
+//         }
+//         client.distube.stop(message);
+//         const embed = new MessageEmbed()
+//         .setDescription('Stopped!')
+//         .setColor("BLUE")
+//         message.channel.send(embed)
+//
+//     }
+// }
