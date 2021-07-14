388ab5a05d909ce39765e96f16b7c2c1df00cbe4
DevJakey <62852156+DevJakey@users.noreply.github.com>
2021-07-14 13:56:09 +0100

    just added a tiny little thing.

--- commands/info/credits.js
+++ commands/info/credits.js
@@ -14,8 +14,8 @@ module.exports = {
     credit4 = client.users.cache.get("805729557357133826"); 
     credit5 = client.users.cache.get("582012181638414357"); // developer
     credit6 = client.users.cache.get("537875750955778058");
-    credit7 = client.users.cache.get("708635694154776657");
-    credit8 = client.users.cache.get("510166383523528705");
+    credit7 = client.users.cache.get("708635694154776657"); // developer
+    credit8 = client.users.cache.get("510166383523528705"); // developer
 
     const embedCredits = new MessageEmbed()
       .setColor("BLURPLE")
