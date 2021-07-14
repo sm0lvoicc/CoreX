1cc74f94c0bd7b02f29469d8f5a1f252640c2955
DevJakey <62852156+DevJakey@users.noreply.github.com>
2021-07-14 12:50:23 +0100

    update credits.js

--- commands/info/credits.js
+++ commands/info/credits.js
@@ -8,12 +8,14 @@ module.exports = {
   userPerms: ["SEND_MESSAGES"],
   clientPerms: ["SEND_MESSAGES"],
   run: async (client, message, args) => {
-    credit1 = client.users.cache.get("451202806653648936");
-    credit2 = client.users.cache.get("538668078012039168");
-    credit3 = client.users.cache.get("533776358980059137");
-    credit4 = client.users.cache.get("805729557357133826");
-    credit5 = client.users.cache.get("582012181638414357");
+    credit1 = client.users.cache.get("451202806653648936"); // developer
+    credit2 = client.users.cache.get("538668078012039168"); // developer
+    credit3 = client.users.cache.get("533776358980059137"); 
+    credit4 = client.users.cache.get("805729557357133826"); 
+    credit5 = client.users.cache.get("582012181638414357"); // developer
     credit6 = client.users.cache.get("537875750955778058");
+    credit7 = client.users.cache.get("708635694154776657"); // developer
+    credit8 = client.users.cache.get("510166383523528705"); // developer
 
     const embedCredits = new MessageEmbed()
       .setColor("BLURPLE")
@@ -32,6 +34,19 @@ module.exports = {
           value:
             "Core Developer of CoreX, got the idea of making CoreX, also join his [Server](https://discord.gg/cKUrkEtMkA) ",
         },
+          {
+          name: credit5.tag,
+          value:
+            "Amazing Developer, She has helped Polish CoreX and find many bugs in it, join her [Server](https://discord.gg/single)",
+        },
+          {
+          name: credit7.tag,
+          value: "Core Developer of CoreX, He has helped with adding new commands and thinking of new additions we could add, and a very nice guy.",
+        },
+          {
+          name: credit8.tag,
+          value: "Core Developer of CoreX, He is the newest addition to the developer team, and I cant imagine how well the bot will be now he's here.",
+        },
         {
           name: credit3.tag,
           value:
@@ -41,11 +56,6 @@ module.exports = {
           name: credit4.tag,
           value:
             "He has helped grow CoreX and make it a better bot, also join his [Server](https://discord.gg/cCp7yGzvBG)",
-        },
-        {
-          name: credit5.tag,
-          value:
-            "Amazing Developer, She has helped Polish CoreX and find many bugs in it, join her [Server](https://discord.gg/single)",
         },
         {
           name: credit6.tag,
