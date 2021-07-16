c900e355efe51572ddc71406e180474f263aa2ea
Tagliatellelol <mohd.a.nada@gmail.com>
2021-07-16 22:37:53 +0400

    I am fixing

--- events/debug,js
+++
@@ -1,5 +0,0 @@
-const client = require('../index')
-
-client.on('debug', async (debug) => {
-    console.log(`client => ${debug}`)
-})
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Collection, Client, MessageEmbed, Message } = require("discord.js");
 require("discord-reply");
-//require(`./dashboard/server`)
 const mongoose = require("mongoose");
 const config = require("./config.json");
 const prefix = config.bot.prefix;
@@ -18,11 +17,7 @@ require("discord-buttons")(client);
 client.commands = new Collection();
 client.aliases = new Collection();
 client.snipes = new Collection();
-require("dotenv").config()
-
-
-
-
+require("dotenv").config();
 
 //top.gg stuff
 const Topgg = require("@top-gg/sdk");
@@ -87,25 +82,4 @@ client.prefix = async function (message) {
   }
   return custom;
 };
-// const { Player } = require("discord-player");
-// client.player = new Player(client, {
-//   leaveOnEnd: true,
-//   leaveOnEndCooldown: 60000,
-//   leaveOnEmpty: true,
-//   leaveOnEmptyCooldown: 60000,
-//   autoSelfDeaf: true,
-//   enableLive: true,
-
-// });
-// client.filters = require('./filters.json').filters;
-// const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
-
-// for (const file of player) {
-//     const event = require(`./player/${file}`);
-//     client.player.on(file.split(".")[0], event.bind(null, client));
-// };
-
-
-// Starboard Feature for On Message Reaction Add
-
 client.login(token);
